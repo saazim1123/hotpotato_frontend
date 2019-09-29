aab0ea02e5e576446d5eb8ce86b3ede6d3d87aad
saazim1123 <saazim1123@gmail.com>
2019-09-29 23:48:51 +0100

    OK so far

--- src/actions/index.js
+++ src/actions/index.js
@@ -5,7 +5,7 @@ const token = localStorage.getItem('token');
 const headers = {
   'Content-Type': 'application/json',
   Accepts: 'application/json',
-  Authorization: token
+  Authorizatidon: token
 };
 
 
