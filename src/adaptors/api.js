0e87b3f25b96adcfff88f1774afe521276b13242
saazim1123 <saazim1123@gmail.com>
2019-10-01 18:16:42 +0100

    Dont merge since Rods login is working

--- src/adaptors/api.js
+++ src/adaptors/api.js
@@ -1,5 +1,7 @@
 const API_ROOT = 'http://localhost:3000/api/v1'
-const API_USER_ROOT = 'http://localhost:3000/api/v1/users'
+const API_USER_ROOT = `${API_ROOT}/users`
+const API_LOGIN_URL = `${API_ROOT}/login`
+const API_VALIDATE_URL = `${API_ROOT}/validate`
 const API_QUESTIONS = `${API_ROOT}/questions`
 
 export default class API {
@@ -10,6 +12,9 @@ export default class API {
         .catch(error => alert(error.message))
   }
   
+
+
+
   static createUser = user => {
     return fetch(API_USER_ROOT, {
       method: 'POST',
@@ -18,4 +23,29 @@ export default class API {
     })
     .then(resp => resp.json())
   }
+
+  static login = user => {
+    return fetch(API_LOGIN_URL, {
+      
+    })
+  }
+
+  static jsonify = res => {
+
+    if (res.ok) return res.json();
+    else throw res.json();
+  };
+
+   static handleServerError = response => {
+    throw response;
+  };
+
+  static saveToken = data => {
+    localStorage.setItem("token", data.token);
+    return data.user;
+  };
+
+ 
+  
+  
 }
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import API_ROOT from '../adaptors/api'
+// import API_ROOT from '../adaptors/api'
 
 export default class Login extends React.Component {
 
@@ -12,24 +12,28 @@ export default class Login extends React.Component {
         }
     }
 
-    //make a login function
+    
 
-    login = (username, password) => {
-        fetch(`${API_ROOT}auth`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                Authorization: localStorage.getItem('token')
-            },
-            body: JSON.stringify({username, password})
-        }).then(res => res.json())
-        .then(res => {
-            localStorage.setItem('token', res.id)
-        })
-    }
+//     //make a login function
+
+//     login = (username, password) => {
+//         fetch(`${API_ROOT}auth`, {
+//             method: 'POST',
+//             headers: {
+//                 'Content-Type': 'application/json',
+//                 Authorization: localStorage.getItem('token')
+//             },
+//             body: JSON.stringify({username, password})
+//         }).then(res => res.json())
+//         .then(res => {
+//             localStorage.setItem('token', res.id)
+//         })
+//     }
     
    
        
+
+ 
 
     handleChange = e => {
         const newFields = {...this.state.fields, [e.target.name]: e.target.value}
@@ -39,8 +43,8 @@ export default class Login extends React.Component {
  
     handleSubmit = e => {
       e.preventDefault();
-      this.login(this.state.fields.username, this.state.fields.password)
-      this.props.history.push('/dashboard')
+    //   this.login(this.state.fields.username, this.state.fields.password)
+      this.props.history.push('/play')
     }
 
 
@@ -49,9 +53,9 @@ export default class Login extends React.Component {
         return (
             <div> 
                 {/* <h1> This is the Login Page </h1> */}
-                <div className="ui form">
-
-          <form className={ "login-form" } onSubmit={this.handleSubmit}>
+            <div className="ui form">
+            
+            <form className={ "login-form" } onSubmit={this.handleSubmit}>
             <div className="ui field">
               <label>Username</label>
               <input
@@ -76,4 +80,50 @@ export default class Login extends React.Component {
             </div>        
         </div>     
     )}
+
+    // render (){
+    //     const {fields} = this.state
+    //     return (
+    //         <div> 
+    //             {/* <h1> This is the Login Page </h1> */}
+    //         <div className="ui form">
+            
+    //         <form className={ "login-form" } onSubmit={this.handleSubmit}>
+    //         <div className="ui field">
+    //           <label>Username</label>
+    //           <input
+    //             name="username"
+    //             placeholder="username"
+    //             value={fields.username}
+    //             onChange={this.handleChange}
+    //             />
+    //         </div>
+    //          <div className="ui field">
+    //             <label>Password</label>
+    //             <input
+    //               name="password"
+    //               type="password"
+    //               placeholder="password"
+    //               value={fields.password}
+    //               onChange={this.handleChange}
+    //             />
+    //             </div>
+    //             <button type="submit" className="ui basic green button" >Submit</button>
+    //         </form>
+    //         </div>        
+    //     </div>     
+    // )}
 }
+
+
+
+
+// NEW 
+
+
+
+
+  
+
+
+// edit form to take in correct format of dob
