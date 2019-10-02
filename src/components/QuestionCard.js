9d527094f06e73fddfddf34c3a8297e038a4f4db
Roderick Cardenas <roderickcardenas1@gmail.com>
2019-10-02 09:37:53 +0100

    setHigh score working

--- src/components/QuestionCard.js
+++ src/components/QuestionCard.js
@@ -1,12 +1,13 @@
 import React from 'react'
 
 const QuestionCard = (props) => {
-    const { question, increaseCurrentScore, nextQuestion, resetScore, setHighestScore, currentScore } = props
+
+    const { question } = props
 
     const answerCheck = (answer) => {
+        const { question, increaseCurrentScore, resetScore, nextQuestion } = props
         if (answer === question.correct_answer) {
             increaseCurrentScore()
-            setHighestScore(currentScore)
             nextQuestion()
             console.log("Correct!")
         } else {
@@ -14,13 +15,14 @@ const QuestionCard = (props) => {
             resetScore()
         }
     }
-
-    return <div className={"question-card"}>
+        
+    return (<div className={"question-card"}>
             <h1 className={"question"}>{question.text}</h1>
             <button className={"answer-btn"} onClick={() => answerCheck(question.first_choice)}>{question.first_choice}</button>
             <button className={"answer-btn"} onClick={() => answerCheck(question.second_choice)}>{question.second_choice}</button>
             <button className={"answer-btn"} onClick={() => answerCheck(question.third_choice)}>{question.third_choice}</button>
            </div>
+           )
 }
 
 export default QuestionCard
